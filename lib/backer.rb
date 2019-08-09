b1ef191aca6660d40f2fc70ccf5cd95ddf60cd60
Steven Hunal <stevehunal@gmail.com>
2019-08-09 00:52:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,6 @@ class Backer
   end 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self) unless project.backers.include?(self)
+    project.add_backer(self)
   end
 end 
